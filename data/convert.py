e9fb3551928645bca5874035c2e4470fb9a4162b
Artem <updatethatpl0x@gmail.com>
2022-03-18 13:11:40 +0800

    added data but also ignored the major files it since theyre 300mb

---
+++ data/convert.py
@@ -0,0 +1,24 @@
+def convert(imgf, labelf, outf, n):
+    f = open(imgf, "rb")
+    o = open(outf, "w")
+    l = open(labelf, "rb")
+
+    f.read(16)
+    l.read(8)
+    images = []
+
+    for i in range(n):
+        image = [ord(l.read(1))]
+        for j in range(28*28):
+            image.append(ord(f.read(1)))
+        images.append(image)
+
+    for image in images:
+        o.write(",".join(str(pix) for pix in image)+"\n")
+    print('done')	
+    f.close()
+    o.close()
+    l.close()
+
+convert("t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte",
+        "mnist_test.csv", 10000)
